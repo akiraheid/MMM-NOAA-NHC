73ae6bcae3be0c70d39e69363adf89084ad8e836
Akira Heid <akira710@protonmail.com>
2018-11-18 21:39:24 -0500

    Use tabs instead of spaces

--- MMM-NOAA-NHC.js
+++ MMM-NOAA-NHC.js
@@ -9,101 +9,101 @@
 // eslint-disable-next-line no-undef
 Module.register('MMM-NOAA-NHC', {
 
-    defaults: {
-        showOnlyActive: true,
-        showPacific: true,
-        showAtlantic: true,
-        updateInterval: 60 * 60 * 1000, // Every hour
-    },
+	defaults: {
+		showOnlyActive: true,
+		showPacific: true,
+		showAtlantic: true,
+		updateInterval: 60 * 60 * 1000, // Every hour
+	},
 
 
-    start: function() {
-        // Log is defined in the Magic Mirror repo
-        // eslint-disable-next-line no-undef
-        Log.info('Starting module: ' + this.name)
+	start: function() {
+		// Log is defined in the Magic Mirror repo
+		// eslint-disable-next-line no-undef
+		Log.info('Starting module: ' + this.name)
 
-        if (this.data.classes === 'MMM-NOAA-NHC') {
-            this.data.classes = 'bright medium'
-        }
+		if (this.data.classes === 'MMM-NOAA-NHC') {
+			this.data.classes = 'bright medium'
+		}
 
-        // Set up the local values, here we construct the request url to use
-        this.loaded = false
-        this.tropicalGraphicalURL = 'https://www.nhc.noaa.gov/gtwo.xml'
+		// Set up the local values, here we construct the request url to use
+		this.loaded = false
+		this.tropicalGraphicalURL = 'https://www.nhc.noaa.gov/gtwo.xml'
 
-        // Trigger the first request
-        this.getData(this)
-    },
-
-
-    getData: function(that) {
-        // Make the initial request to the helper then set up the timer to perform
-        // the updates
-        that.sendSocketNotification(
-            'GET-TROPICAL-DATA', that.tropicalGraphicalURL)
-
-        setTimeout(that.getData, that.config.updateInterval, that)
-    },
+		// Trigger the first request
+		this.getData(this)
+	},
 
 
-    getStyles: function() {
-        return ['MMM-NOAA-NHC.css']
-    },
+	getData: function(that) {
+		// Make the initial request to the helper then set up the timer to perform
+		// the updates
+		that.sendSocketNotification(
+			'GET-TROPICAL-DATA', that.tropicalGraphicalURL)
+
+		setTimeout(that.getData, that.config.updateInterval, that)
+	},
 
 
-    getDom: function() {
-        // Set up the local wrapper
-        const wrapper = document.createElement('div')
-
-        if (this.loaded) {
-            const row = document.createElement('tr')
-            if (this.config.showPacific) {
-                const column = document.createElement('td')
-                if (this.result.pacificActive) {
-                    const img = document.createElement('img')
-                    img.setAttribute('src', 'https://www.nhc.noaa.gov/xgtwo/resize/two_pac_5d0_resize.gif')
-                    img.setAttribute('alt', 'Could not load Pacific image')
-                    column.appendChild(img)
-                } else {
-                    const span = document.createElement('span')
-                    span.innerHTML = 'No<br/>Pacific<br/>Activity'
-                    span.className = 'small no-activity'
-                    column.appendChild(span)
-                }
-                row.appendChild(column)
-            }
-
-            if (this.config.showAtlantic) {
-                const column = document.createElement('td')
-                if (this.result.atlanticActive) {
-                    const img = document.createElement('img')
-                    img.setAttribute('src', 'https://www.nhc.noaa.gov/xgtwo/resize/two_atl_5d0_resize.gif')
-                    img.setAttribute('alt', 'Could not load Atlantic image')
-                    column.appendChild(img)
-                } else {
-                    const span = document.createElement('span')
-                    span.innerHTML = 'No<br/>Atlantic<br/>Activity'
-                    span.className = 'small no-activity'
-                    column.appendChild(span)
-                }
-                row.appendChild(column)
-            }
-
-            const imageTable = document.createElement('table')
-            imageTable.appendChild(row)
-            wrapper.appendChild(imageTable)
-        } else {
-            wrapper.innerHTML = 'Loading NOAA NHC data...'
-        }
-
-        return wrapper
-    },
+	getStyles: function() {
+		return ['MMM-NOAA-NHC.css']
+	},
 
 
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GOT-TROPICAL-DATA') {
-            this.loaded = true
-            this.result = payload.result
-            this.updateDom(1000)
-        }
-    }
+	getDom: function() {
+		// Set up the local wrapper
+		const wrapper = document.createElement('div')
+
+		if (this.loaded) {
+			const row = document.createElement('tr')
+			if (this.config.showPacific) {
+				const column = document.createElement('td')
+				if (this.result.pacificActive) {
+					const img = document.createElement('img')
+					img.setAttribute('src', 'https://www.nhc.noaa.gov/xgtwo/resize/two_pac_5d0_resize.gif')
+					img.setAttribute('alt', 'Could not load Pacific image')
+					column.appendChild(img)
+				} else {
+					const span = document.createElement('span')
+					span.innerHTML = 'No<br/>Pacific<br/>Activity'
+					span.className = 'small no-activity'
+					column.appendChild(span)
+				}
+				row.appendChild(column)
+			}
+
+			if (this.config.showAtlantic) {
+				const column = document.createElement('td')
+				if (this.result.atlanticActive) {
+					const img = document.createElement('img')
+					img.setAttribute('src', 'https://www.nhc.noaa.gov/xgtwo/resize/two_atl_5d0_resize.gif')
+					img.setAttribute('alt', 'Could not load Atlantic image')
+					column.appendChild(img)
+				} else {
+					const span = document.createElement('span')
+					span.innerHTML = 'No<br/>Atlantic<br/>Activity'
+					span.className = 'small no-activity'
+					column.appendChild(span)
+				}
+				row.appendChild(column)
+			}
+
+			const imageTable = document.createElement('table')
+			imageTable.appendChild(row)
+			wrapper.appendChild(imageTable)
+		} else {
+			wrapper.innerHTML = 'Loading NOAA NHC data...'
+		}
+
+		return wrapper
+	},
+
+
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'GOT-TROPICAL-DATA') {
+			this.loaded = true
+			this.result = payload.result
+			this.updateDom(1000)
+		}
+	}
 })
--- node_helper.js
+++ node_helper.js
@@ -11,45 +11,45 @@ const DOMParser = require('xmldom').DOMParser
 
 module.exports = NodeHelper.create({
 
-    start: function () {
-        // Set up local values
-        this.result = null
-    },
+	start: function () {
+		// Set up local values
+		this.result = null
+	},
 
 
-    getData: function(payload) {
-        const that = this
-        this.url = payload
+	getData: function(payload) {
+		const that = this
+		this.url = payload
 
-        request({url: this.url, method: 'GET'}, function(error, response, body) {
-            if (!error && response.statusCode == 200) {
-                const result = {}
-                const doc = new DOMParser().parseFromString(body)
-                const descriptions = doc.getElementsByTagName('description')
+		request({url: this.url, method: 'GET'}, function(error, response, body) {
+			if (!error && response.statusCode == 200) {
+				const result = {}
+				const doc = new DOMParser().parseFromString(body)
+				const descriptions = doc.getElementsByTagName('description')
 
-                result.atlanticActive = that.isAreaActive(descriptions[2].textContent)
-                result.pacificActive = that.isAreaActive(descriptions[3].textContent)
-                that.result = result
-            }
+				result.atlanticActive = that.isAreaActive(descriptions[2].textContent)
+				result.pacificActive = that.isAreaActive(descriptions[3].textContent)
+				that.result = result
+			}
 
-            that.sendSocketNotification(
-                'GOT-TROPICAL-DATA', {'url': that.url, 'result': that.result})
-        })
-    },
-
-
-    isAreaActive: function(cdata) {
-        // Parse CDATA text and determine if there is activity
-        const doc = new DOMParser().parseFromString(cdata)
-        const text = doc.getElementsByTagName('div')[1].textContent
-        return text.search(/formation is not expected/) == -1
-    },
+			that.sendSocketNotification(
+				'GOT-TROPICAL-DATA', {'url': that.url, 'result': that.result})
+		})
+	},
 
 
-    socketNotificationReceived: function(notification, payload) {
-        if (notification === 'GET-TROPICAL-DATA') {
-            this.getData(payload)
-        }
-    }
+	isAreaActive: function(cdata) {
+		// Parse CDATA text and determine if there is activity
+		const doc = new DOMParser().parseFromString(cdata)
+		const text = doc.getElementsByTagName('div')[1].textContent
+		return text.search(/formation is not expected/) == -1
+	},
+
+
+	socketNotificationReceived: function(notification, payload) {
+		if (notification === 'GET-TROPICAL-DATA') {
+			this.getData(payload)
+		}
+	}
 
 })
